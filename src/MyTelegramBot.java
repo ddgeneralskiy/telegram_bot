5cf963bedec57ff9c4ee5dcf497f181a6591358f
admin <dimitriy-93@mail.ru>
2019-02-17 20:17:02 +0300

    создан класс для бота

---
+++ src/MyTelegramBot.java
@@ -0,0 +1,22 @@
+package ru.thematdev.bot;
+
+//import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+}
